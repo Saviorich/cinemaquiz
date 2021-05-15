b945eef8219da95e885bf64ef5de6552f713902b
Saviorich <defalt3874@gmail.com>
2021-05-16 00:54:59 +0300

    Initial commit

--- src/main/java/by/bntu/fitr/model/dao/QuizDao.java
+++ src/main/java/by/bntu/fitr/model/dao/QuizDao.java
@@ -1,9 +1,10 @@
 package by.bntu.fitr.model.dao;
 
+import by.bntu.fitr.model.dao.exception.DaoException;
 import by.bntu.fitr.model.entity.Quiz;
 
 import java.util.List;
 
 public interface QuizDao {
-    List<Quiz> findAll();
+    List<Quiz> findAll() throws DaoException;
 }
--- src/main/java/by/bntu/fitr/model/service/QuizServiceImpl.java
+++ src/main/java/by/bntu/fitr/model/service/QuizServiceImpl.java
@@ -2,7 +2,10 @@ package by.bntu.fitr.model.service;
 
 import by.bntu.fitr.model.dao.DaoProvider;
 import by.bntu.fitr.model.dao.QuizDao;
+import by.bntu.fitr.model.dao.exception.DaoException;
+import by.bntu.fitr.model.entity.Question;
 import by.bntu.fitr.model.entity.Quiz;
+import by.bntu.fitr.model.service.exception.ServiceException;
 
 import java.util.List;
 
@@ -11,12 +14,23 @@ public class QuizServiceImpl implements QuizService {
     private static final QuizDao quizDao = DaoProvider.getInstance().getQuizDao();
 
     @Override
-    public List<Quiz> findAll() {
-        return null;
+    public List<Quiz> findAll() throws ServiceException {
+        try {
+            return quizDao.findAll();
+        } catch (DaoException e) {
+            throw new ServiceException();
+        }
     }
 
     @Override
     public double calculatePercentage(Quiz quiz) {
-        return 0;
+        int quizSize = quiz.getQuestionList().size();
+        int correctAmount = 0;
+        for (Question question : quiz.getQuestionList()) {
+            if (question.isCorrect()) {
+                correctAmount++;
+            }
+        }
+        return (double) correctAmount / quizSize;
     }
 }
