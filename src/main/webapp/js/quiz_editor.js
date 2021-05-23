0b46bd8b8710d9d058b7d40464016dde3eba9ccf
Saviorich <defalt3874@gmail.com>
2021-05-23 15:09:32 +0300

    change quiz editor

--- src/main/webapp/js/quiz_editor.js
+++ src/main/webapp/js/quiz_editor.js
@@ -2,7 +2,7 @@ function selectOnChange() {
     var textToAdd = '';
 
     var end = $('#s1 option:selected').text();
-    for (let i = 0; i < parseInt(end); i++) {
+    for (let i = 1; i <= parseInt(end); i++) {
         textToAdd += createQuestionCreator(i)
     }
     $('.creator_block').html(textToAdd)
@@ -11,7 +11,7 @@ function selectOnChange() {
 function a(doc, number) {
     if (doc.checked) {
         $(`.question_creator_body${number}`).html(
-            `<span><label for="i2">Answer:<input id="i2" name="correct_answer" class="writable_answer" type="text"></label></span>`
+            `<span style="margin: 10px"><label for="i2">Answer:<input id="i2" name="correct_answer" class="writable_answer" type="text"></label></span>`
         );
     } else {
         $(`.question_creator_body${number}`).html(
@@ -37,6 +37,6 @@ function createQuestionCreator(number) {
                     <span class="option"><input name="r${number}" type="radio" value="4" required> Option 4: <input name="Option4ForQuestion${number}" type="text"><br/></span>
                 </div>
                 <hr>
-                <span class="creator_footer"><label for="i3${number}">Writable:<input id="i3${number}" type="checkbox" class="type" name="question_type" onchange="a(this, ${number})"></label></span>
+                <span class="creator_footer"><label for="i3${number}">Writable:<input id="i3${number}" type="checkbox" class="type" name="question_type${number}" onchange="a(this, ${number})"></label></span>
             </div>`
 }
